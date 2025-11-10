1873ba3c94615f1c6d1340e6ad7680881be65186
x80486 <x80486@mailo.com>
2025-11-10 12:34:45 -0500

    Add startup debug output for configuration parameters and key application values

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-export ELECTRON_OZONE_PLATFORM_HINT="auto" # https://www.electronjs.org/docs/latest/api/environment-variables#electron_ozone_platform_hint-linux
-export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+set -o errexit -o nounset -o pipefail
 
-printf "Current Desktop Environment: [%s]\n" "${XDG_CURRENT_DESKTOP}"
-printf "Current Session Type: [%s]\n" "${XDG_SESSION_TYPE}"
-printf "Running Wire Desktop with the following arguments: [%s]\n" "$@"
+ADDITIONAL_ARGS=()
 
-exec zypak-wrapper "/app/Wire/wire-desktop" "$@"
+if [[ -n "${FLATPAK_ID:-}" && -n "${XDG_RUNTIME_DIR:-}" ]]; then
+  export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+fi
+
+if [[ "${XDG_SESSION_TYPE:-}" == "wayland" ]]; then
+  # TODO: Rework this when application upgrades to Electron 38
+  ADDITIONAL_ARGS+=("--enable-features=UseOzonePlatform")
+  ADDITIONAL_ARGS+=("--enable-wayland-ime")
+  ADDITIONAL_ARGS+=("--ozone-platform-hint=auto")
+  ADDITIONAL_ARGS+=("--wayland-text-input-version=3")
+fi
+
+if [[ "${WIRE_DEBUG_ENABLED:-}" == "true" ]]; then
+  printf "ADDITIONAL_ARGS: [%s]\n" "${ADDITIONAL_ARGS[*]:-}"
+  printf "TMPDIR: %s\n" "${TMPDIR:-}"
+  printf "XDG_CURRENT_DESKTOP: %s\n" "${XDG_CURRENT_DESKTOP:-}"
+  printf "XDG_SESSION_TYPE: %s\n" "${XDG_SESSION_TYPE:-}"
+fi
+
+exec zypak-wrapper "/app/wire-desktop" "${ADDITIONAL_ARGS[@]}" "$@"
