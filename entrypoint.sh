b1798822f19cb3b72bb2bc2076d1afed77ccb4d9
zacaj <zacaj@zacaj.com>
2026-05-29 17:38:57 -0400

    Narrow fix to only add --disable-features=WaylandFractionalScaleV1

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ if [[ -n "${FLATPAK_ID:-}" && -n "${XDG_RUNTIME_DIR:-}" ]]; then
 fi
 
 if [[ "${XDG_SESSION_TYPE:-}" == "wayland" ]]; then
+  # TODO: Rework this when application upgrades to Electron 38
+  ADDITIONAL_ARGS+=("--enable-features=UseOzonePlatform")
   ADDITIONAL_ARGS+=("--enable-wayland-ime")
   ADDITIONAL_ARGS+=("--ozone-platform-hint=auto")
   ADDITIONAL_ARGS+=("--wayland-text-input-version=3")
